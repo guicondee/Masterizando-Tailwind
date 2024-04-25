39465847d57dbb3ff629d34502ce058869a2e3a0
Guilherme vinicios conde <guicondee@outlook.com>
2024-04-25 09:10:17 -0300

    removendo outline do component input

--- src/app/components/Form/Input/input.tsx
+++ src/app/components/Form/Input/input.tsx
@@ -1,4 +1,3 @@
-import { Search } from "lucide-react";
 import { ComponentProps } from "react";
 
 interface InputPrefixProps extends ComponentProps<"div"> {}
@@ -12,7 +11,7 @@ interface InputControlProps extends ComponentProps<"input"> {}
 export function InputControl(props: InputControlProps) {
   return (
     <input
-      className=" w-full border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600"
+      className="w-full border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600 outline-none"
       {...props}
     />
   );
@@ -23,7 +22,7 @@ interface InputRootProps extends ComponentProps<"div"> {}
 export function InputRoot(props: InputRootProps) {
   return (
     <div
-      className="mx-1 flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm"
+      className="flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm focus-within:border-green-300 focus-within:ring-2 focus-within:ring-green-100"
       {...props}
     />
   );

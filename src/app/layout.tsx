3ebda0e2cc2d3b46a5c4becdd0f4de340ca98c8c
Guilherme vinicios conde <guicondee@outlook.com>
2024-06-05 09:30:53 -0300

    change on body className

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html className="" lang="en">
+    <html lang="en">
       <body className={inter.className}>
         <div className="min-h-screen lg:grid lg:grid-cols-app dark:bg-zinc-900">
           <Sidebar />
